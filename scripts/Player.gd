2a50cd2520d97309caa66383545635d471b0a85d
ozumCanBulundu <ozumcanbulundu@gmail.com>
2022-08-02 15:49:03 +0300

    Jump update

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,15 +1,18 @@
 extends KinematicBody2D
 
-export var gravityConstant = 30;
+export var gravityConstant = 150;
 var velocity:Vector2 = Vector2.ZERO
 var direction:Vector2 = Vector2.ZERO
 export var speed :int = 30
+export var jumpConstant : int = 3000
 
 func _physics_process(delta: float) -> void:
 	gravity()
 	get_move_vector()
 	velocity.x = direction.x * speed
-	velocity = move_and_slide(velocity)
+	_jump()
+	velocity = move_and_slide(velocity, Vector2.UP)
+
 	
 
 func gravity():
@@ -18,8 +21,11 @@ func gravity():
 
 func get_move_vector():
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	direction.y = -1 if Input.is_action_just_pressed("jump") and is_on_floor() else 0
-	
-#	if (direction.x == 0):
-#		velocity.x = lerp(0, velocity.x, pow(2,-50 * get_process_delta_time()))
+	direction.y = -1 if Input.is_action_just_pressed("jump") and is_on_floor() else 1
 	return direction
+
+func _jump():
+	if(direction.y == -1.0):
+		velocity.y = jumpConstant * direction.y * get_physics_process_delta_time()
+
+	
