8ba8c96be66fbf870c061f56959d0a2fe8167b7f
ozumCanBulundu <ozumcanbulundu@gmail.com>
2022-08-06 17:13:55 +0300

    Updated camera

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,14 +5,25 @@ var velocity = Vector2.ZERO
 export var speed = 45
 export var jumpConstant = 130
 var is_falling = false
+
+var screen_width:int = 64
+var distance :float = 1
+var cameraSpeed: = 0.03
+var new_camera_position
+
 func _physics_process(delta):
 	is_falling = velocity.y>0
 	var input = Input.get_axis("move_left","move_right")
 	velocity.x = input*speed
+	
 	if input==1:
 		$Sprite.flip_h=false
+		$Camera2D.offset_h =min(($Camera2D.offset_h + cameraSpeed), (self.position.x + distance - screen_width/2) * delta) 
+		
 	if input==-1:
 		$Sprite.flip_h=true
+		$Camera2D.offset_h =min(($Camera2D.offset_h - cameraSpeed) , (self.position.x - distance - screen_width/2) * delta) 
+		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y=-jumpConstant
 	if is_falling:
@@ -20,5 +31,6 @@ func _physics_process(delta):
 	else:
 		velocity.y+=gravityConstant
 	velocity=move_and_slide(velocity,Vector2.UP)
+	
 	
 	
